fc886e6df0cfcd5748731bca98e15228b9fa68a4
Ethan Kang <ekang78@gatech.edu>
2024-06-24 04:44:07 +0000

    set renv config options for package manager repo

--- scripts/00-0-vscode-R-lib.R
+++ scripts/00-0-vscode-R-lib.R
@@ -1,3 +1,10 @@
 # VSCode-R library cache dir
 Sys.setenv('RENV_PATHS_CACHE' = '/workspaces/reserve/.cache/R/vscode-R/v5/R-4.3/x86_64-conda-linux-gnu')
-renv::init(project = '/workspaces/reserve/lib/vscode-R')
+options(
+  repos = c(
+    CRAN = 'https://packagemanager.posit.co/cran/__linux__/bookworm/latest'
+  ),
+  renv.config.ppm.enabled = TRUE,
+  renv.config.repos.override = 'https://packagemanager.posit.co/cran/__linux__/bookworm/latest'
+)
+renv::init(project = '/workspaces/reserve/lib/vscode-R', repos = getOption('repos'))
--- scripts/00-1-renv-lib.R
+++ scripts/00-1-renv-lib.R
@@ -1,3 +1,10 @@
 # project library cache dir
 Sys.setenv('RENV_PATHS_CACHE' = '/workspaces/reserve/.cache/R/renv/v5/R-4.3/x86_64-conda-linux-gnu')
+options(
+  repos = c(
+    CRAN = 'https://packagemanager.posit.co/cran/__linux__/bookworm/latest'
+  ),
+  renv.config.ppm.enabled = TRUE,
+  renv.config.repos.override = 'https://packagemanager.posit.co/cran/__linux__/bookworm/latest'
+)
 renv::init()
