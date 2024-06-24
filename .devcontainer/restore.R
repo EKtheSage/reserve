9a8ef9c0a0c4605ea66eb0d30b16e6edb18a3f5c
Ethan Kang <ethan.yskang@gmail.com>
2024-06-24 08:59:34 +0000

    remove trailing comma

--- .devcontainer/restore.R
+++ .devcontainer/restore.R
@@ -1,6 +1,6 @@
 options(
   repos = c(
-    CRAN = 'https://packagemanager.posit.co/cran/__linux__/bookworm/latest',
+    CRAN = 'https://packagemanager.posit.co/cran/__linux__/bookworm/latest'
   ),
   renv.config.ppm.enabled = TRUE,
   renv.config.repos.override = 'https://packagemanager.posit.co/cran/__linux__/bookworm/latest'
