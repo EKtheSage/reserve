38a35373bba7b11ce7c3228bc395e97eed2a6103
Ethan Kang <ethan.yskang@gmail.com>
2024-06-28 00:35:35 +0000

    added STAN_HAS_CXX17=true to the post Create bash script so stan can use CXX17 standard

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,4 +6,13 @@ micromamba activate reserve
 
 export RENV_PATHS_CACHE="$PWD/.cache/R/renv"
 
+# Path to the make/local file
+MAKE_LOCAL_FILE="$CONDA_PREFIX/bin/cmdstan/make/local"
+
+# Check if the line already exists in the make/local file
+# append this flag to local stan environment variables file
+if ! grep -Fxq "STAN_HAS_CXX17=true" "$MAKE_LOCAL_FILE"; then
+    echo "STAN_HAS_CXX17=true" >> "$MAKE_LOCAL_FILE"
+fi
+
 Rscript /tmp/restore.R
