9d4bae552ad078597e15dbd22de0c500db7dc5f7
Ethan Kang <ethan.yskang@gmail.com>
2024-07-03 04:50:16 +0000

    may not need this to have cpp 17 in R.

---
+++ scripts/edit-cpp-config.R
@@ -0,0 +1,7 @@
+dotR <- file.path(Sys.getenv("HOME"), ".R")
+if (!file.exists(dotR)) dir.create(dotR)
+M <- file.path(dotR, "Makevars")
+if (!file.exists(M)) file.create(M)
+cat("\nCXX17FLAGS=-O3 -march=native -mtune=native -fPIC",
+    "CXX17=g++", # or clang++ but you may need a version postfix
+    file = M, sep = "\n", append = TRUE)
